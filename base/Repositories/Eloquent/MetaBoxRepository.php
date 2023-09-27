da5fc7ffbe82ae1fa5223eb7a2643e46345abc63
Tec-Dynamics <support@tecdynamics.co.uk>
2023-09-27 09:14:58 +0100

    PHP-8

---
+++ base/Repositories/Eloquent/MetaBoxRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tec\Base\Repositories\Eloquent;
+
+use Tec\Base\Repositories\Interfaces\MetaBoxInterface;
+use Tec\Support\Repositories\Eloquent\RepositoriesAbstract;
+
+class MetaBoxRepository extends RepositoriesAbstract implements MetaBoxInterface
+{
+}
---
+++ base/Repositories/Interfaces/MetaBoxInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tec\Base\Repositories\Interfaces;
+
+use Tec\Support\Repositories\Interfaces\RepositoryInterface;
+
+interface MetaBoxInterface extends RepositoryInterface
+{
+}
