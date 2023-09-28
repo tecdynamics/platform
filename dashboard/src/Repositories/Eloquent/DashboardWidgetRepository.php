c003183b1723f976b4cb76aab130b862c4410f2c
Tec-Dynamics <support@tecdynamics.co.uk>
2023-09-28 14:32:32 +0100

    Add files via upload

---
+++ dashboard/src/Repositories/Eloquent/DashboardWidgetRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tec\Dashboard\Repositories\Eloquent;
+
+use Tec\Dashboard\Repositories\Interfaces\DashboardWidgetInterface;
+use Tec\Support\Repositories\Eloquent\RepositoriesAbstract;
+
+class DashboardWidgetRepository extends RepositoriesAbstract implements DashboardWidgetInterface
+{
+}
---
+++ dashboard/src/Repositories/Interfaces/DashboardWidgetInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tec\Dashboard\Repositories\Interfaces;
+
+use Tec\Support\Repositories\Interfaces\RepositoryInterface;
+
+interface DashboardWidgetInterface extends RepositoryInterface
+{
+}
