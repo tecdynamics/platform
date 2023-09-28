806f8a930a831987dc677519a8a83794d8046e91
Tec-Dynamics <support@tecdynamics.co.uk>
2023-09-28 14:19:47 +0100

    Delete setting directory

--- setting/src/Repositories/Interfaces/SettingInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tec\Setting\Repositories\Interfaces;
-
-use Tec\Support\Repositories\Interfaces\RepositoryInterface;
-
-interface SettingInterface extends RepositoryInterface
-{
-}
