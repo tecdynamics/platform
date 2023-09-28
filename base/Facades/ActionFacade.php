e40b8ffea0fc41d97cb968ee4b112bec75eae560
Tec-Dynamics <support@tecdynamics.co.uk>
2023-09-28 11:53:28 +0100

    Add files via upload

---
+++ base/Facades/ActionFacade.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tec\Base\Facades;
+
+use Tec\Base\Supports\Action;
+use Illuminate\Support\Facades\Facade;
+
+class ActionFacade extends Facade
+{
+
+    /**
+     * @return string
+     * @since 2.1
+     */
+    protected static function getFacadeAccessor()
+    {
+        return Action::class;
+    }
+}
---
+++ base/Facades/AssetsFacade.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tec\Base\Facades;
+
+use Tec\Base\Supports\Assets;
+use Illuminate\Support\Facades\Facade;
+
+class AssetsFacade extends Facade
+{
+
+    /**
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return Assets::class;
+    }
+}
