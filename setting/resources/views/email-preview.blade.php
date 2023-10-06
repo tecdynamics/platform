527201b4741623c8e15b7c391acc80cc1c543e72
Tec-Dynamics <support@tecdynamics.co.uk>
2023-10-06 10:01:43 +0100

    Update email-preview.blade.php

--- setting/resources/views/email-preview.blade.php
+++ setting/resources/views/email-preview.blade.php
@@ -8,15 +8,13 @@
       $header = str_replace(['{{ site_url }}','{{ site_logo }}','{{ site_title }}','{{ date_time }}'],[$site_url,$site_logo,$site_title,$date_time],
       apply_filters(BASE_FILTER_EMAIL_TEMPLATE_HEADER, get_setting_email_template_content('core', 'base', 'header')));
        $footer= str_replace(['{{ site_title }}','{{ date_year }}','{{ date_time }}'],[$site_title,$date_year,$date_time],
-apply_filters(BASE_FILTER_EMAIL_TEMPLATE_FOOTER, get_setting_email_template_content('core', 'base', 'footer'))); 
+apply_filters(BASE_FILTER_EMAIL_TEMPLATE_FOOTER, get_setting_email_template_content('core', 'base', 'footer')));
+       $content= str_replace(['{{ header }}','{{ footer }}'],'',$emailContent);
  @endphp
  <div class="col-12 mb-4 p-2 ">
      <footer class="d-flex justify-content-between"> <small class="text-sm">Note: The Following Preview is not 100% accurate because of the CMS style conflicts</small>
  <button type="button" class="btn btn-outline-primary btn-sm preview-email" >Preview</button></footer>
  </div>
-
-    {!! $header  !!}
-
-<div id="display-area" >{!! $emailContent !!}</div>
-
+ {!! $header !!}
+<div id="display-area" >{!! $content !!}</div>
  {!! $footer !!}
