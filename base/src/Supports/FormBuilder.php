6e8220d7aa8f1a974766fe499ba88849ab0725a7
Tec-Dynamics <support@tecdynamics.co.uk>
2024-06-30 11:31:54 +0100

    Update FormBuilder.php

--- base/src/Supports/FormBuilder.php
+++ base/src/Supports/FormBuilder.php
@@ -55,7 +55,7 @@ class FormBuilder
         $this->url = $url;
         $this->html = $html;
         $this->view = $view;
-        $this->csrfToken = $csrfToken;
+        $this->csrfToken =(string) $csrfToken;
         $this->request = $request;
     }
 
